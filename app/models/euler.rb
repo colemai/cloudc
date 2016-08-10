cf68f580ac28733f32d6ad669130b1abc32402a0
colemai <colemai@tcd.ie>
2016-08-09 19:12:37 -0700

    working, pre-clean

--- app/models/euler.rb
+++ app/models/euler.rb
@@ -1,6 +1,8 @@
 class Euler < ActiveRecord::Base
 
 	@e = 2718281828459045235360287471352662497757247093699959574966967627724076630353547594571382178525166427427466391932003059921817413596629043572900334295260595630738132328627
+	$prime = 0
+
 
 	def self.is_prime?(n)
 	   Math.sqrt(n).floor.downto(2).each {|i| return false if n % i == 0}
@@ -18,10 +20,15 @@ class Euler < ActiveRecord::Base
 	        break if arr.length == x
 	    end
 	    arr[x-1]
+	    
 	end
 
 	def self.mite(a,b)
-		a
+		if a == Euler.ite(Euler.last.x,Euler.last.y)
+			return a
+		elsif b == Euler.ite(Euler.last.x,Euler.last.y)
+			return b
+		end
 	end
 
 end
